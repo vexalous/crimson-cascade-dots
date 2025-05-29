cb89fe966e0dca817ff55a47da6b5906b8206b27
vexalous <vexalous@hotmail.com>
2025-05-29 16:53:51 -0400

    Create volume_notify.sh

---
+++ hypr/scripts/volume_notify.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+CRIMSON="#DC143C"
+LIGHT_GRAY="#cccccc"
+NEAR_BLACK="#0a0a0a"
+ICON_MUTED="/usr/share/icons/Papirus-Dark/48x48/panel/audio-volume-muted.svg"
+ICON_LOW="/usr/share/icons/Papirus-Dark/48x48/panel/audio-volume-low.svg"
+ICON_MEDIUM="/usr/share/icons/Papirus-Dark/48x48/panel/audio-volume-medium.svg"
+ICON_HIGH="/usr/share/icons/Papirus-Dark/48x48/panel/audio-volume-high.svg"
+ICON_MIC_MUTED="/usr/share/icons/Papirus-Dark/48x48/devices/audio-input-microphone-muted.svg"
+ICON_MIC="/usr/share/icons/Papirus-Dark/48x48/devices/audio-input-microphone.svg"
+
+get_v(){ pactl get-sink-volume @DEFAULT_SINK@|grep -Po '[0-9]+(?=%)'|head -n 1;}
+is_m(){ pactl get-sink-mute @DEFAULT_SINK@|grep -q yes;}
+is_mic_m(){ pactl get-source-mute @DEFAULT_SOURCE@|grep -q yes;}
+
+if [[ "$1" == MUTE ]];then 
+    N="Microphone"
+    if is_mic_m;then 
+        I=$ICON_MIC_MUTED
+        T="Mic Muted"
+        P=0
+    else 
+        I=$ICON_MIC
+        T="Mic On"
+        P=100
+    fi
+else 
+    N="Volume"
+    V=$(get_v)
+    if is_m || [ "$V" -eq 0 ];then 
+        I=$ICON_MUTED
+        T="Muted"
+        P=0
+    elif [ "$V" -lt 34 ];then 
+        I=$ICON_LOW
+        T="${V}%"
+        P=$V
+    elif [ "$V" -lt 67 ];then 
+        I=$ICON_MEDIUM
+        T="${V}%"
+        P=$V
+    else 
+        I=$ICON_HIGH
+        T="${V}%"
+        P=$V
+    fi
+fi
+notify-send -h string:x-canonical-private-synchronous:vol_notif -h int:value:$P -u low -i "$I" -a "$N" "$T" --hint=string:fgcolor:$LIGHT_GRAY,string:bgcolor:$NEAR_BLACK,string:hlcolor:$CRIMSON
