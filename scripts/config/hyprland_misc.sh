cb1c282f7050d26f4dad7fc94d75fa2b41340a14
vexalous <vexalous@hotmail.com>
2025-05-29 20:52:04 -0400

    Create hyprland_misc.sh

---
+++ scripts/config/hyprland_misc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+TARGET_FILE="$HYPR_CONF_TARGET_DIR/misc.conf"
+
+echo "Generating $TARGET_FILE..."
+mkdir -p "$(dirname "$TARGET_FILE")"
+
+cat << EOF > "$TARGET_FILE"
+misc {
+    disable_hyprland_logo = true
+    disable_splash_rendering = true
+    mouse_move_enables_dpms = true
+    enable_swallow = true
+    swallow_regex = ^(Alacritty|kitty|foot|wezterm)$
+    new_window_takes_over_fullscreen = 2
+    focus_on_activate = true
+    background_color = rgba(0a0a0aff)
+}
+EOF
+
+echo "$TARGET_FILE generated."
