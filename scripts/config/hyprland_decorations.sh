0182120957a7ad24d0eb68eada3f05f037c04490
vexalous <vexalous@hotmail.com>
2025-05-29 20:52:18 -0400

    Create hyprland_decorations.sh

---
+++ scripts/config/hyprland_decorations.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TARGET_FILE="$HYPR_CONF_TARGET_DIR/decorations.conf"
+
+echo "Generating $TARGET_FILE..."
+mkdir -p "$(dirname "$TARGET_FILE")"
+
+cat << EOF > "$TARGET_FILE"
+decoration {
+    rounding = 8
+    blur {
+        enabled = true; size = 6; passes = 3; new_optimizations = true;
+        xray = true; noise = 0.015; contrast = 1.0; brightness = 0.95;
+    }
+}
+EOF
+
+echo "$TARGET_FILE generated."
